62de75f9bd1c22b8341313ba52b1201d27e42ad1
VKPYpy <49330371+VKPYpy@users.noreply.github.com>
2019-05-11 20:30:45 +0700

    Add files via upload

---
+++ precache-manifest.e15efc5371b5d323008e6424ae790558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f64494ee56c84ddf1cd5ab6907a8aef",
+    "url": "/static/media/persik.5f64494e.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "bd102cbe95115227c110",
+    "url": "/static/js/main.bd102cbe.chunk.js"
+  },
+  {
+    "revision": "48de42b1744e3a290082",
+    "url": "/static/js/2.48de42b1.chunk.js"
+  },
+  {
+    "revision": "bd102cbe95115227c110",
+    "url": "/static/css/main.9bcd9804.chunk.css"
+  },
+  {
+    "revision": "48de42b1744e3a290082",
+    "url": "/static/css/2.2fb5c15d.chunk.css"
+  },
+  {
+    "revision": "3a6658d4e632a8d8fd424d88775704f8",
+    "url": "/index.html"
+  }
+];
